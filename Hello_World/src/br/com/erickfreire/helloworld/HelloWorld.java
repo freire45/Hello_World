069a60b63ffc8b95c4ba86b6ef737383115bb8f3
Erick Freire <erilefr@gmail.com>
2022-09-28 23:46:22 -0300

    a
    a

--- Hello_World/src/br/com/erickfreire/helloworld/HelloWorld.java
+++ Hello_World/src/br/com/erickfreire/helloworld/HelloWorld.java
@@ -8,7 +8,7 @@ package br.com.erickfreire.helloworld;
 
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("Hellow World!");
+		System.out.println("Hello World!");
 	}
 
 }
