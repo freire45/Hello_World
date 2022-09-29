66cc50a2645f6695867f8e9b107205663652fb9e
Erick Freire <erilefr@gmail.com>
2022-09-28 23:42:51 -0300

    Hello World
    Hello World

---
+++ Hello_World/src/br/com/erickfreire/helloworld/HelloWorld.java
@@ -0,0 +1,14 @@
+package br.com.erickfreire.helloworld;
+
+/**
+ * Hello World in Java
+ * @author Erick Freire
+ * @version 1 - Created in 09-28-2022
+ */
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hellow World!");
+	}
+
+}
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# Hello_World
- Hello World In Java
+# Hello World In Java
+  Hello World In Java
+
+ ## Print do Programa Funcionando
+![Currículo no Computador](helloWorld.png)
+
+## Video: 
+https://www.youtube.com/watch?v=PE2OcB0_Q0g
